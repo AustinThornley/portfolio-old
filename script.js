f997ed24b85b9c72f8ad3d07c6946583de9759d8
Austin Thornley <austinjt1012@gmail.com>
2021-07-04 23:56:13 -0600

    updated styling/layout

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-// This program can check how often you need to do an audit to reach a daily goal. It will also tell you if you are on track or not.
+/*
+
+This program can check how often you need to do an audit to reach a daily goal. It will also tell you if you are on track or not.
+Made by: Austin Thornley 2021
+
+*/
+
+// Declaring needed variables
 let dailyAudits;
 let dailyHours;
 let currentAudits;
 let hoursWorked;
 let onTrack;
 
-// add output for hourlyRate
-//adding output for HourlyRate, plus github link to code
-
-
+// grabs all values from input on fields onClick
 function getValues() {
-	dailyAudits = document.getElementById('dailyAudits').value;
-	dailyHours = document.getElementById('dailyHours').value;
-	currentAudits = document.getElementById('currentAudits').value;
-	hoursWorked = document.getElementById('hoursWorked').value;
-	checkAudits(dailyAudits,dailyHours,currentAudits,hoursWorked);
+  dailyAudits = document.getElementById('dailyAudits').value;
+  dailyHours = document.getElementById('dailyHours').value;
+  currentAudits = document.getElementById('currentAudits').value;
+  hoursWorked = document.getElementById('hoursWorked').value;
+  checkAudits(dailyAudits, dailyHours, currentAudits, hoursWorked);
 }
 
 //function calculates all audit stats
@@ -26,13 +30,19 @@ function checkAudits(dailyAudits, dailyHours, currentAudits, hoursWorked) {
   let firstNum = Math.trunc(timePerAudit); // gets first num
 
   // Checks whether or not you are on track
-  if (currentAudits < (auditsPerHour * hoursWorked)) {
-	onTrack = `You are not on track! ⛔️`;
+  if (currentAudits < auditsPerHour * hoursWorked) {
+    onTrack = `You are not on track! ⛔️`;
   } else {
-	onTrack = `You are on track to hit ${dailyAudits}! ✅`;
+    onTrack = `You are on track to hit ${dailyAudits}! ✅`;
   }
 
-  document.getElementById('results').textContent = `You will need to do 1 audit every ${firstNum} minutes and ${parsedInt} seconds in order to hit ${dailyAudits} audits in a ${dailyHours} hour shift.`;
-  document.getElementById('speed').textContent = `${onTrack}`;
-  document.getElementById('results2').textContent = `Daily Audits: ${dailyAudits} | Daily Hours: ${dailyHours} | Current Audits: ${currentAudits} | Hours Worked: ${hoursWorked} | Hourly Audits Goal: ${auditsPerHour}`;
+
+  // Outputs all elements (inputted by user or calculated values)
+  document.getElementById('onTrack').textContent = `${onTrack}`;
+  document.getElementById('auditString').textContent = `You will need to do 1 audit every ${firstNum} minutes and ${parsedInt} seconds in order to hit ${dailyAudits} audits in a ${dailyHours} hour shift.`;
+  document.getElementById('dailyAuditsVal').textContent = `Daily Audits: ${dailyAudits}`;
+  document.getElementById('dailyHoursVal').textContent = `Daily Hours: ${dailyHours}`;  
+  document.getElementById('currentAuditsVal').textContent = `Current Audits: ${currentAudits}`;
+  document.getElementById('hoursWorkedVal').textContent = `Hours Worked: ${hoursWorked}`;
+  document.getElementById('auditsPerHourVal').textContent = `Hourly Audits Goal: ${auditsPerHour}`;
 }
